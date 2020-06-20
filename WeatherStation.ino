df72f282607647af80ba0aa4b68b4a4c98f4add3
ihor-chaban <igor.chaban.1995@gmail.com>
2020-06-20 23:40:37 +0300

    v1.1

--- WeatherStation.ino
+++ WeatherStation.ino
@@ -1,7 +1,13 @@
-///////////////////////////////////////////
-//  WeatherStation with weather predict  //
-//         Author - Ihor Chaban          //
-///////////////////////////////////////////
+/*
+    Weather Station with prediction v1.1
+      - fixed out of range issue during MNK calculation
+      - added map function for decimal values
+      - added sensor information to the error screen
+      - added debug mode
+      - some code refactoring
+    Author: Ihor Chaban
+    Jun 2020
+*/
 
 #include <Adafruit_BMP085.h>
 #include <DHT.h>
@@ -11,10 +17,12 @@
 
 // Software settings
 #define SERVO_INVERT          1
-#define SERVO_MIN             0
+#define SERVO_MIN             10
 #define SERVO_MAX             180
 #define STANDBY_TIME          10000
-#define MEASURMENTS_PER_HOUR  120
+#define MEASURMENTS_PER_HOUR  60
+#define MMHG_TO_PASCAL        133.322
+#define DEBUG                 0
 
 // Hardware settings
 #define DHT_PIN               3
@@ -29,26 +37,26 @@
 #define DHTPIN                DHT_PIN
 
 #if (SERVO_INVERT == 0)
-#define SERVO_LEFT SERVO_MIN
+#define SERVO_LEFT  SERVO_MIN
 #define SERVO_RIGHT SERVO_MAX
 #else
-#define SERVO_LEFT SERVO_MAX
+#define SERVO_LEFT  SERVO_MAX
 #define SERVO_RIGHT SERVO_MIN
 #endif
+#define INTERVAL (3600000 / MEASURMENTS_PER_HOUR)
 
 Servo servo;
 Adafruit_BMP085 bmp;
 DHT dht(DHT_PIN, DHTTYPE);
 LiquidCrystal_I2C lcd(LCD_ADDRESS, LCD_WIDTH, LCD_HEIGHT);
-const unsigned long interval = 3600000 / MEASURMENTS_PER_HOUR;
 unsigned long pressure, pressure_array[MEASURMENTS_PER_HOUR], measure_timer, backlight_timer;
 byte time_array[MEASURMENTS_PER_HOUR];
 float temperature, humidity;
 int angle, last_angle;
 bool backlight_flag;
 
-enum enum_custom_chars {sun_1, sun_2, cloud_1, cloud_2, termometr, droplet, celsius, barometr};
-const byte custom_chars[8][8] = {
+enum icons {sun_1, sun_2, cloud_1, cloud_2, termometr, droplet, celsius, barometr};
+const byte icons_bytes[8][8] = {
   {0x03, 0x07, 0x0F, 0x0F, 0x0F, 0x0F, 0x07, 0x03},
   {0x18, 0x1C, 0x1E, 0x1E, 0x1E, 0x1E, 0x1C, 0x18},
   {0x00, 0x06, 0x09, 0x12, 0x10, 0x0F, 0x05, 0x0A},
@@ -60,9 +68,9 @@ const byte custom_chars[8][8] = {
 };
 
 void setup() {
-  // Debug mode
-  // Serial.begin(9600);
-
+#if (DEBUG)
+  Serial.begin(9600);
+#endif
   Wire.begin();
   pinMode(SERVO_POWER_PIN, OUTPUT);
   pinMode(BACKLIGHT_BUTTON_PIN, INPUT_PULLUP);
@@ -77,7 +85,7 @@ void setup() {
   lcd.setCursor(0, 1);
   lcd.print("by Gorus");
   for (byte i = 0; i <= barometr; i++) {
-    lcd.createChar(i, custom_chars[i]);
+    lcd.createChar(i, icons_bytes[i]);
   }
   lcd.setCursor(LCD_WIDTH - 2, 0);
   lcd.write(sun_1);
@@ -113,17 +121,17 @@ void setup() {
   PrintValuesToLCD();
   backlight_flag = true;
   backlight_timer = millis();
-
-  // Debug mode
-  // Serial.println();
+#if (DEBUG)
+  Serial.println();
+#endif
 }
 
 void loop() {
-  if (millis() - measure_timer >= interval) {
+  if (millis() - measure_timer >= INTERVAL) {
+    measure_timer = millis();
     pressure = ReadAveragePressure();
     temperature = dht.readTemperature();
     humidity = dht.readHumidity();
-    measure_timer = millis();
     Wire.beginTransmission(BMP180_ADDRESS);
     if (Wire.endTransmission()) {
       ShowErrorScreen(true);
@@ -137,7 +145,7 @@ void loop() {
     pressure_array[MEASURMENTS_PER_HOUR - 1] = pressure;
     PrintValuesToLCD();
     if (pressure_array[0]) {
-      angle = map(CalculateMNKCoefficient() * MEASURMENTS_PER_HOUR, -250, 250, SERVO_LEFT, SERVO_RIGHT);
+      angle = round(mapf(CalculateMNKCoefficient() * MEASURMENTS_PER_HOUR, -250, 250, SERVO_LEFT, SERVO_RIGHT));
       angle = constrain(angle, min(SERVO_LEFT, SERVO_RIGHT), max(SERVO_LEFT, SERVO_RIGHT));
       if (angle != last_angle) {
         digitalWrite(SERVO_POWER_PIN, HIGH);
@@ -147,14 +155,14 @@ void loop() {
         digitalWrite(SERVO_POWER_PIN, LOW);
         last_angle = angle;
       }
-
-      // Debug mode
-      // Serial.println("Set servo on angle " + String(angle));
+#if (DEBUG)
+      Serial.println("Set servo on angle " + String(angle));
+#endif
     }
-    // Debug mode
-    // Serial.println();
+#if (DEBUG)
+    Serial.println();
+#endif
   }
-
   if (!digitalRead(BACKLIGHT_BUTTON_PIN)) {
     if (!backlight_flag) {
       backlight_flag = true;
@@ -162,18 +170,10 @@ void loop() {
     }
     backlight_timer = millis();
   }
-  if (backlight_flag && (millis() - backlight_timer >= STANDBY_TIME)) {
+  if (backlight_flag && millis() - backlight_timer >= STANDBY_TIME) {
     backlight_flag = false;
     lcd.noBacklight();
   }
-}
-
-unsigned long ReadAveragePressure() {
-  unsigned long sum = 0;
-  for (byte i = 0; i < 10; i++) {
-    sum += bmp.readPressure();
-  }
-  return (round(sum / 10.0));
 }
 
 float CalculateMNKCoefficient() {
@@ -182,20 +182,28 @@ float CalculateMNKCoefficient() {
   unsigned long sum_y = 0;
   unsigned long sum_x2 = 0;
   unsigned long sum_xy = 0;
+  long temp = 0;
   for (byte i = 0; i < MEASURMENTS_PER_HOUR; i++) {
     sum_x += time_array[i];
     sum_y += pressure_array[i];
     sum_x2 += time_array[i] * time_array[i];
     sum_xy += time_array[i] * pressure_array[i];
   }
-  result = MEASURMENTS_PER_HOUR * sum_xy;
-  result = result - (sum_x * sum_y);
-  result = (float)result / ((MEASURMENTS_PER_HOUR * sum_x2) - (sum_x * sum_x));
-
-  // Debug mode
-  // Serial.println("MNK coefficient is " + String(result));
-
+  temp = MEASURMENTS_PER_HOUR * sum_xy;
+  temp = temp - (sum_x * sum_y);
+  result = (float)temp / ((MEASURMENTS_PER_HOUR * sum_x2) - (sum_x * sum_x));
+#if (DEBUG)
+  Serial.print("MNK coefficient is " + String(result));
+#endif
   return result;
+}
+
+unsigned long ReadAveragePressure() {
+  unsigned long sum = 0;
+  for (byte i = 0; i < 10; i++) {
+    sum += bmp.readPressure();
+  }
+  return (round(sum / 10.0) - 90000);
 }
 
 void PrintValuesToLCD() {
@@ -221,17 +229,17 @@ void PrintValuesToLCD() {
   lcd.setCursor((LCD_WIDTH / 2) - 7, 1);
   lcd.write(barometr);
   lcd.print(" ");
-  temp_string = String(pressure / 133.32237);
+  temp_string = String((pressure + 90000) / MMHG_TO_PASCAL);
   lcd.print(temp_string);
   lcd.print(" mm Hg");
-
-  // Debug mode
-  //  Serial.println("Pressure array");
-  //  for (byte i = 0; i < MEASURMENTS_PER_HOUR; i++) {
-  //    Serial.println(String(time_array[i]) + "\t" + String(pressure_array[i]));
-  //  }
-  //  Serial.println("Temperature " + String(temperature) + "°C, Humidity " +
-  //                 String(humidity) + "%, Pressure " + String(pressure / 133.32237) + " mm Hg");
+#if (DEBUG)
+  Serial.println("Pressure array");
+  for (byte i = 0; i < MEASURMENTS_PER_HOUR; i++) {
+    Serial.println("\"" + String(time_array[i]) + "\",\"" + String(pressure_array[i]) + "\"");
+  }
+  Serial.println("Temperature " + String(temperature) + "°C, Humidity " +
+                 String(humidity) + "%, Pressure " + String((pressure + 90000) / MMHG_TO_PASCAL) + " mm Hg");
+#endif
 }
 
 void ShowErrorScreen(bool bmp_or_dht) {
@@ -239,14 +247,20 @@ void ShowErrorScreen(bool bmp_or_dht) {
   lcd.setCursor(0, 0);
   lcd.print("     Error!     ");
   lcd.setCursor(0, 1);
-  lcd.print("Sensor ");
-  lcd.print((bmp_or_dht) ? "1" : "2");
-  lcd.print(" is down");
-
-  // Debug mode
-  // Serial.println("Error! " + String((bmp_or_dht) ? "BMP" : "DHT") + " sensor is down");
-
-  while (true) {
-  }
+  lcd.print((bmp_or_dht) ? "BMP" : "DHT");
+  lcd.print(" sens is down");
+  lcd.backlight();
+  digitalWrite(SERVO_POWER_PIN, HIGH);
+  delay(1000);
+  servo.write(round((SERVO_LEFT + SERVO_RIGHT) / 2.0));
+  delay(1000);
+  digitalWrite(SERVO_POWER_PIN, LOW);
+#if (DEBUG)
+  Serial.println("Error! " + String((bmp_or_dht) ? "BMP" : "DHT") + " sensor is down");
+#endif
+  while (true) {}
 }
 
+float mapf(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
